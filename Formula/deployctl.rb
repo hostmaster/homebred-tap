fbaaab378a938ec1ba3388c5279c788163a414b6
Igor Khomyakov <homyakov@gmail.com>
2016-09-09 17:47:36 +0300

    fix install

--- Formula/deployctl.rb
+++ Formula/deployctl.rb
@@ -2,19 +2,19 @@ class Deployctl < Formula
   desc "Deployment control tool"
   homepage "https://cbxp.in/7854d21e46b0de9a9592/"
   url "https://cbxp.in/7854d21e46b0de9a9592/deployctl.osx"
-  sha256 "3cfa8f41e740c0dc09d854f3833058caec0ea0d67d19e950f97eee61106b0daf"
+  sha256 "4896989edb8416b40b3fb0d53c483fd34737b2270c168a56422e55539dd24a60"
+  version "7854d21e46b0de9a9592"
 
-#   depends_on "go" => :build
-
-#  def install
-#    system "make", "install", "DESTDIR=#{prefix}"
-#  end
-
+  def install
+    mv "deployctl.osx", "deployctl"
+    bin.install "deployctl"
+  end
+  
   def caveats
     "Finish setup by following: https://gist.github.com/hostmaster/33cd9aa55af894dbfa7a"
   end
 
   test do
-    assert_match "showconf", shell_output("#{bin}/deployctl")
+    system "deployctl"
   end
 end
